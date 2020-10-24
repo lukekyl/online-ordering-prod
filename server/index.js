8fdd5a49130704d02167eeee254afd8d4859aaaf
Kyle Luke <luke.kylew@gmail.com>
2020-10-24 12:30:01 -0600

    Added cors to express server.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 
+
 const app = express();
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
