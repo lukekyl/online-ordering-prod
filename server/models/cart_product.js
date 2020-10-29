62441887ccd897d9c9b2090efe7f4c70b48896bb
Kyle Luke <luke.kylew@gmail.com>
2020-10-28 21:55:15 -0600

    Added cart-product join table to db with sequelize.

---
+++ server/models/cart_product.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cart_product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Cart_product.belongsTo(models.Cart, {
+        foreignKey: 'cart_id',
+        onDelete: 'CASCADE'
+      })
+      Cart_product.belongsTo(models.Product, {
+        foreignKey: 'product_id',
+        onDelete: 'CASCADE'
+      })
+    }
+  };
+  Cart_product.init({
+    cart_id: DataTypes.INTEGER,
+    product_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Cart_product',
+  });
+  return Cart_product;
+};
