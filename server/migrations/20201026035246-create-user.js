5a23c443393ac0a3751a32fb99484b5f9e0833bf
Kyle Luke <luke.kylew@gmail.com>
2020-11-03 20:52:35 -0700

    Added seed files for each model.

--- server/migrations/20201026035246-create-user.js
+++ server/migrations/20201026035246-create-user.js
@@ -15,7 +15,9 @@ module.exports = {
         type: Sequelize.STRING
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        // unique: true,
+        // allowNull: false
       },
       password: {
         type: Sequelize.STRING
---
+++ server/seeds/20201104013818-user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('User', [{
+      name: DataTypes.STRING,
+      username: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      address: DataTypes.STRING,
+      payment_id: DataTypes.INTEGER
+    }], {});
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
