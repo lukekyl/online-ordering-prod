34869e1b306decc31893b0cb157d8964f414cb5c
Kyle Luke <luke.kylew@gmail.com>
2020-11-13 19:59:29 -0700

    Issues with localhost proxy and ECONNREFUSED error.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
 import config from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import pino from 'express-pino-logger'
 
-const express = require('express');
-const userRoutes = require(`./routes/userRoutes`)
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
+import userRoutes from './routes/userRoutes'
+
+
 
 config.config();
 
-
 const app = express();
+
 app.use(cors())
-app.use(express.json())
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
-// app.use(userRoutes);
+app.use(userRoutes);
 app.listen(process.env.DB_PORT, () =>
     console.log('Express server is running on localhost:3001')
 );
@@ -29,3 +31,13 @@ app.get('/api/greeting', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
+
+export default app;
+
+// const express = require('express');
+// const routes = require('./routes/userRoutes');
+// const Parser = require('body-parser');
+// const app = express();
+// app.use(Parser.urlencoded({extended:true}));
+// app.use(routes);
+// app.listen(process.env.API_PORT);
