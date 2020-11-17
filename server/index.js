98d88fd878f5195c26c880085239a1d4288ea493
Kyle Luke <luke.kylew@gmail.com>
2020-11-16 21:15:04 -0700

    Express server fixed, had to remove cors call from server index. Now able to get and post responses to server from react app.

--- server/index.js
+++ server/index.js
@@ -1,29 +1,33 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors')
+// const cors = require('cors');
 const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')
+const pino = require('express-pino-logger')();
 
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(cors())
+// app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
 app.use(userRoutes);
-app.listen(process.env.DB_PORT, () =>
-    console.log('Express server is running on localhost:3001')
-);
 
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to this API.',
-}));
+const port = process.env.DB_PORT;
+
+app.listen(port, () =>
+    console.log(`Express server is running on localhost:${port}`)
+);
 
 app.get('/api/greeting', (req, res) => {
     const name = req.query.name || 'World';
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to this API.',
+}));
+    
 });
