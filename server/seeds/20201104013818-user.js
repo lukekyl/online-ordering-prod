b52f08811de270081320382b374c78400ab0a904
Kyle Luke <luke.kylew@gmail.com>
2020-11-19 19:46:15 -0700

    Unknown Error when seeding DB.

--- server/seeds/20201104013818-user.js
+++ server/seeds/20201104013818-user.js
@@ -3,15 +3,6 @@ const fake = require('faker');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
 
     let users = []
     for(let i=0; i<-10;i++){
