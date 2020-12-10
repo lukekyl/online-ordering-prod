5197067de7f89f978fa84b53f678dffb6434eee8
Kyle Luke <luke.kylew@gmail.com>
2020-12-09 22:23:07 -0700

    Added and configured Tailwind CSS to React frontend.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Index from './pages/index.js';
 import * as serviceWorker from './serviceWorker';
 
 // Dinero will be money display converter, add to App component
+// Tailwind CSS for CSS Framework, with CSS Modules or Styled Components for CSS-in-JS Solution
 
 ReactDOM.render(
   <React.StrictMode>
