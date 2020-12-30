684ef82d93a1c38c1aa58c563fd1ec38091093cd
Kyle Luke <luke.kylew@gmail.com>
2020-12-29 19:20:12 -0700

    Added Category component as sub component to menu.

--- src/components/menuCategory.js
+++ src/components/menuCategory.js
@@ -2,7 +2,8 @@ import React from "react"
 import Product from './menuProduct'
 
 const Category = (props) => {
-
+// If called, there is at least one product in this category. 
+// Iterate through all products in selected category and create a product card for each.
     
     return (
         <>
