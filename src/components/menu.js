a61227ac58533c5191ce791f8498fc724a73e81a
Kyle Luke <luke.kylew@gmail.com>
2021-01-09 20:45:45 -0700

    Have fetch loading products. Working on async loading of products into state correctly.

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,25 +5,38 @@ import { createState, useState } from '@hookstate/core';
 
 const globalState = createState(0)
 
-const resourcePath = 'http://localhost:3001/product';
-    const fetchMenu = async () => await fetch(resourcePath)
-        .then(r => {
-            console.log(r.json())
-            return r.json()
-        })
-    globalState.set(fetchMenu)
+
     
 
     
 
 const Menu = () => {
-// Fetch menu from backend. Iterate through all categories and create category component for each.
-    const state = useState(globalState);
+// Fetch menu from backend. Iterate through all categories and create category component for each.    
+    const resourcePath = 'http://localhost:3001/product';
+    const fetchMenu = () => fetch(resourcePath)
+        .then(response => response.json())
+        .then(products => {
+            console.log(products)
+            return products
+        })
+
+    const state = useState(new Promise(fetchMenu));
 
     // console.log(state)
+    if (state.promised) {
+        return <p>Loading {resourcePath}</p>;
+    }
+
+    if (state.error) {
+        return <p>Failed to load {resourcePath}<br />
+            <code style={{ color: 'red' }}>{state.error.toString()}</code>
+        </p>
+    }
+
+
     return (
         <>
-            {/* <p>{state.value}</p> */}
+            <p key=''>Loaded {resourcePath}</p>
         </>
     );
 }
