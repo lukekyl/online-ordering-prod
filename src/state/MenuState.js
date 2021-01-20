8e29e78f30cca3487a0d06dd3e4d6a5f87604fc7
Kyle Luke <luke.kylew@gmail.com>
2021-01-19 22:54:00 -0700

    Added menu sorting functionality to menustate.

--- src/state/MenuState.js
+++ src/state/MenuState.js
@@ -7,19 +7,40 @@ export const resourcePath = 'http://localhost:3001/product';
 const fetchMenu = async () => fetch(resourcePath)
     .then(response => response.json())
     .then(productList => {
-        products = productList
-        return products
+        let menu = sortMenu(productList)
+        
+        return menu
     })
 
-let products = []
-let globalState = createState(products)
+const sortMenu = (menu) => {
+        let categories = []
+        const sort = menu.map(product => {
+                if (!categories.filter(c => c.name === product.category).length > 0){
+                    let category = {
+                        name: product.category,
+                        products: []
+                    }
+                    category.products.push(product)
+                    categories.push(category)
+                } else {
+                    let category = categories.filter(c => c.name === product.category)
+                    category[0].products.push(product)
+                }
+                // console.log(product)
+        })
+        // console.log(categories)
+        return categories
+    }    
+
+let menu = []
+let globalState = createState(menu)
 
 
 export const useMenuState = () => {
 // Fetch menu from backend. Iterate through all categories and create category component for each.    
     globalState = useState(fetchMenu)
     const state = useState(globalState);
-
+    // console.log(state)
     return state;
     
 }
