d4aabc62df0fe443e0d4836acafd103ba872c14d
Kyle Luke <luke.kylew@gmail.com>
2021-03-04 21:24:09 -0700

    Removing initial form for testing from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,39 +9,28 @@ class Index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      greeting: ''
+      // name: '',
+      // greeting: ''
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ name: event.target.value });
+    // this.setState({ name: event.target.value });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
+    // fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+    //   .then(response => response.json())
+    //   .then(state => this.setState(state));
   }
 
 
   render() {
   return (
     <Layout>
-        <form onSubmit={this.handleSubmit}>
-        <label htmlFor="name">Enter your name: </label>
-        <input
-          id="name"
-          type="text"
-          value={this.state.name}
-          onChange={this.handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <p>{this.state.greeting}</p>
       <Menu />
     </Layout>
   );
