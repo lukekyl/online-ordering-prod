0139f1800c0a71e405a56e0af19979b1d731fa1d
Kyle Luke <luke.kylew@gmail.com>
2021-03-04 19:42:47 -0700

    Updating product card styling

--- src/components/menuProduct.js
+++ src/components/menuProduct.js
@@ -6,8 +6,8 @@ const Product = ({product}) => {
     // Create product card for menu.
     return (
         <>
-            <div key={product.value.id} className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                <div className="bg-local" style={{backgroundImage: `url(${product.value.image_url})`}}>Image</div>
+            <div key={product.value.id} className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl hover:shadow-2xl hover:border-white transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-local bg-center bg-cover h-60" style={{backgroundImage: `url(${product.value.image_url})`}}></div>
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">    
                     <p><strong>{product.value.name}</strong></p>
                 </div> 
