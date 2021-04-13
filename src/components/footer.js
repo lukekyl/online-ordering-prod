e2f83b3ebcfa613e56de2865d75af5ab4bdbc4c2
Kyle Luke <luke.kylew@gmail.com>
2021-04-12 20:43:57 -0600

    Updating footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Header = () => {
+const Footer = () => {
 
     
     return (
@@ -113,4 +113,4 @@ const Header = () => {
     );
 }
 
-export default Header
+export default Footer
